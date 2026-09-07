3a1bf90e5ce6844813994076a2c58593501204c4
Linda Achieng <achielinda44@gmail.com>
2026-09-07 10:13:47 -0700

    Create skeleton of the tasks for the bot

---
+++ lib/msaidizi/tools/emails.ex
@@ -0,0 +1,12 @@
+defmodule Msaidizi.Tools.Emails do
+ @moduledoc """
+  Reads emails and generates a summary of important things that need
+  to be worked on
+ """
+
+ def run do
+   IO.puts "Fetching emails..."
+
+   :ok
+ end
+end
